5508d216fbe87c6d6008153a59d10e5494b10e9a
SophieMalka <s.malka@live.fr>
2023-05-17 11:37:13 +0200

    Fix bug createBook database

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const bookRoutes = require('./routes/book');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://DevBack:hWnm89db0RGVMcod@cluster0.wklwgjy.mongodb.net/?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://DevBack:qnRslP7UCKdZCSrp@projet7.dgzqwll.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
