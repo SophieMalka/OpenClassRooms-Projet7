c0751b33901fefd798ca826041946ac5383c26c5
SophieMalka <s.malka@live.fr>
2023-05-31 08:49:30 +0200

    Add rate limit signup

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,8 +4,9 @@ const router = express.Router();
 const userCtrl = require('../controllers/user');
 const validEmail = require('../middleware/email-validator');
 const validPassword = require('../middleware/password-validator');
+const rateLimit = require('../middleware/rate-limit');
 
-router.post('/signup', validEmail, validPassword, userCtrl.signup);
-router.post('/login', userCtrl.login)
+router.post('/signup', rateLimit, validEmail, validPassword, userCtrl.signup);
+router.post('/login', rateLimit, userCtrl.login)
 
 module.exports = router;
